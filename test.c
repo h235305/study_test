d496fcc18921877781eb6970f678d023cf5f10e5
h235305 <h3323560600@163.com>
2026-09-13 15:07:51 +0800

    尝试云端修改

--- test.c
+++ test.c
@@ -8,10 +8,11 @@ printf("hello github\n");
 
 printf("try to modefied\n");
 
-int a = 10;
-int b = 15;
+// int a = 10;
+// int b = 15;
 
-printf("This is a num %d\n",a+b);
+// printf("This is a num %d\n",a+b);
+//No,only to read 
 
 return 0;
 
