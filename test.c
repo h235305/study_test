ad0d50656c45829d298b2924fc09b53458de1be5
h235305 <h3323560600@163.com>
2026-09-13 15:06:00 +0800

    新增代码

--- test.c
+++ test.c
@@ -3,7 +3,15 @@
 
 int main()
 {
-printf("hello github");
+
+printf("hello github\n");
+
+printf("try to modefied\n");
+
+int a = 10;
+int b = 15;
+
+printf("This is a num %d\n",a+b);
 
 return 0;
 
