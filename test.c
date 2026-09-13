30b0b3e48665fb8936d87645f437966403f3423c
h235305 <h3323560600@163.com>
2026-09-13 15:15:09 +0800

    尝试云端修改2
    Removed old print statements and added new text.

--- test.c
+++ test.c
@@ -3,18 +3,9 @@
 
 int main()
 {
-githup.com great!
 
-printf("hello github\n");
-
-printf("try to modefied\n");
-
-// int a = 10;
-// int b = 15;
-
-// printf("This is a num %d\n",a+b);
-//No,only to read 
-
+  haha...
+    please again!
 return 0;
 
 }
